98ee7235f6455a67e992fca4a59265cc747f07af
GhostxD <ademolajohn844@gmail.com>
2023-08-13 13:39:04 +0100

    cleanup

--- components/Technologies.jsx
+++ components/Technologies.jsx
@@ -2,15 +2,19 @@ import React from 'react';
 
 const Technologies = () => {
   return (
-    <div className="text-center flex justify-center py-8 text-xl">
-      <ul className="flex gap-8 text-gray-400">
+    <div className="text-center flex justify-center py-8 text-lg">
+      <ul className="flex gap-3 text-gray-400">
         <li>Powered By:</li>
         <li className="flex items-center">
-          <img src="base.webp" alt="" className="w-[27px]" />
+          <img
+            src="https://img.freepik.com/free-photo/golden-yellow-seamless-venetian-plaster-background_24972-294.jpg?w=2000"
+            alt=""
+            className="w-[27px] h-[27px] rounded-full"
+          />
           &nbsp;
-          <p>Base</p>
+          <p className="text-md">Mode Network</p>
         </li>
-        <li className="flex items-center">
+        {/* <li className="flex items-center">
           <img src="zora.png" alt="" className="w-[27px]" />
           &nbsp;
           <p>Zora</p>
@@ -19,7 +23,7 @@ const Technologies = () => {
           <img src="covalent.jpg" alt="" className="w-[27px]" />
           &nbsp;
           <p>Covalent</p>
-        </li>
+        </li> */}
       </ul>
     </div>
   );
